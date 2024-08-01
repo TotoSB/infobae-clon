2f916dcb9f10b0f8c2de6f72b15eaffdba249927
unknown <elmundodetoto2006@gmail.com>
2024-08-01 13:53:48 -0300

    feat: Index and themes development

--- app/views.py
+++ app/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    postings = Posts.objects.all()
+    themes = Themes.objects.all()
+    # images_first = ImagesPosts.objects.get(order=1)
+
+    return render(request, "index.html", {"posts": postings, "themes": themes})
+
+def theme(request, theme):
+    try:
+        theme_get = Themes.objects.get(name=theme)
+        return render(request, "theme.html")
+    except: 
+        return redirect('index')
