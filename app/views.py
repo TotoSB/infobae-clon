61b0614b2ebd697ac12d75d11a94765a5ae6beeb
unknown <elmundodetoto2006@gmail.com>
2024-08-13 13:50:12 -0300

    adasdsa

--- app/views.py
+++ app/views.py
@@ -7,8 +7,8 @@ from django.db.models import Count, Sum
 import requests
 
 def get_global_data():
-    response_dlls = requests.get("https://dolarapi.com/v1/dolares")
-    data_dlls = response_dlls.json()
+    # response_dlls = requests.get("https://dolarapi.com/v1/dolares")
+    # data_dlls = response_dlls.json()
     themes = mainThemes.objects.annotate(
         num_posts=Count('posts')
     ).order_by('-num_posts')[:5]
@@ -21,7 +21,7 @@ def get_global_data():
     return {
         "themes": themes,
         'today': formatted_date,
-        "dollars": data_dlls,
+        # "dollars": data_dlls,
         "all_themes": all_themes,
         "pop_tags": tags,
     }
